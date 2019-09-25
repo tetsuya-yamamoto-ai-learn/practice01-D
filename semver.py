60f725e1895f038d4fafad252f22b54eb04fa9e3
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-25 17:00:21 +0900

    [課題D-2]文字列表現の導入
    - __str__を設定することで文字列表現を入れられることが分かった。
    - __""__のことをなんていうんだろうか。。。

--- semver.py
+++ semver.py
@@ -7,16 +7,17 @@ class SemanticVersion():
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
+    def __str__(self):
+        return f'{self.major}.{self.minor}.{self.patch}'
+
+
+
 
 def main():
     # Python3.7.0 というバージョンを表現したもの
     py370 = SemanticVersion(major=3, minor=7, patch=0)
 
-    # 等価である場合の確認
-    print(SemanticVersion(3, 7, 0) == py370)  # True
-
-    # 等価でない場合の確認
-    print(SemanticVersion(3, 7, 1) == py370)  # False
+    print('3.7.0' == str(py370))  # True
 
 
 if __name__ == '__main__':
