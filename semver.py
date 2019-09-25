d061f3f117fb0c7c00ad0688487d642c0ccbf1a2
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-25 16:46:00 +0900

    First commit
    - 少し書いていますが、とりあえず登録

---
+++ semver.py
@@ -0,0 +1,23 @@
+class SemanticVersion():
+    def __init__(self, major, minor, patch):
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+
+def main():
+    # Python3.7.0 というバージョンを表現したもの
+    py370 = SemanticVersion(major=3, minor=7, patch=0)
+
+    # 等価である場合
+    print(SemanticVersion(3, 7, 0) == py370)  # True
+
+    # 等価でない場合
+    print(SemanticVersion(3, 7, 1) == py370)  # False
+
+
+if __name__ == '__main__':
+    main()
