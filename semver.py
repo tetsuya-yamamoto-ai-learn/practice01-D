daab76c2caf810ff121d51a2a24c719a9aee342c
tetsuya-yamamoto-ai-learn <tetsuya.yamamoto.ai.study@gmail.ocm>
2019-09-25 16:51:56 +0900

    [課題D-1]クラスの等価性の比較
    - __eq__()メソッドを使うことで、インスタンス同士を == で結んだ時の処理を設定できることが分かった。
    - __dict__を使うことで、インスタンス変数すべてを引っ張ってこれることがわかった。
    - オブジェクト内部の便利な機能は__""___みたいな感じになっているようですね。

--- semver.py
+++ semver.py
@@ -12,10 +12,10 @@ def main():
     # Python3.7.0 というバージョンを表現したもの
     py370 = SemanticVersion(major=3, minor=7, patch=0)
 
-    # 等価である場合
+    # 等価である場合の確認
     print(SemanticVersion(3, 7, 0) == py370)  # True
 
-    # 等価でない場合
+    # 等価でない場合の確認
     print(SemanticVersion(3, 7, 1) == py370)  # False
 
 
